b7ad419e1fbdf6ae3f4749f37c5e5241ab02afc6
Candice <candice.gleason@gmail.com>
2021-01-27 14:13:27 -0800

    CSV support
    Add CSV file output support and comments in index.js to explain each part of the application's process

--- README.md
+++ README.md
@@ -4,21 +4,31 @@
 
 > CLI tool to easily export [FunRetro.io](https://funretro.io/) retrospective boards using Playwright
 
+> **Updates (2021/01/27) from original repository:**
+> * [fast-csv](https://www.npmjs.com/package/fast-csv) dependency for CSV formatting
+> * Option to export as CSV or TXT filetype
+> * RegEx validation for user input URL and filetype
+> * Selector for board title changed to ".board-name" as "#board-name" is not present in the document
+> * Comments to better explain how the process works for anyone who wants to work from this repository
+> * For CSV: Board messages with 0 votes are not included in the output file
+
 ## Installing / Getting started
 
 It's required to have [npm](https://www.npmjs.com/get-npm) installed locally to follow the instructions.
+When starting the application you will need to enter a valid URL and either "csv" or "txt" for the filetype you want to output.
+> Example: "https://easyretro.io/publicboard..." "csv" will output MyBoardTitle.csv to the repository directory
 
 ```shell
-git clone https://github.com/julykaz/funretro-export.git
+git clone https://github.com/candisuuu/funretro-export.git
 cd funretro-export
 npm install
-npm start -- "http://funretro.io/board..." "../exported-file.txt"
+npm start -- "https://easyretro.io/publicboard..." "[file extension]"
 ```
 
 ## TODO
-
-- Export card comments
-- More export options (PDF, CSV)
+* Explore possible routes that can decrease the amount of code to compile (2021/01/27)
+* Additional file export options - PDF filetype, file directory (including create new directory), file name (2021/01/27)
+* Explore card comments (2020/08)
 
 ## Licensing
 
--- index.js
+++ index.js
@@ -1,65 +1,191 @@
-const fs = require('fs');
+const fs = require('fs'); // use fs for formatting plaintext files
+const csv = require('fast-csv'); // use fast-csv for formatting csv files
 const path = require('path');
 const { chromium } = require('playwright');
 
-const [url, file] = process.argv.slice(2);
+// define url and extension variables
+const [url, extension] = process.argv.slice(2);
 
-if (!url) {
-  throw 'Please provide a URL as the first argument.';
-}
+// check if the user had entered a valid url
+const urlRegex = new RegExp(/^(https?|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/, 'i');
+const urlTest = url ? urlRegex.exec(url) : null;
 
-async function run() {
+if (!urlTest)
+  throw 'Please provide a valid URL as the first argument.';
+
+// check for and make sure to limit the user to plaintext or CSV extensions
+const extRegex = new RegExp(/^(txt|csv)$/, 'i');
+const extTest = extension ? extRegex.exec(extension) : null;
+
+if (!extTest)
+  throw 'Please provide a filetype you want to use (CSV or TXT) as the second argument.'
+
+/**
+ * Run data scrape and return parsedData object with data configured
+ * based on fileType value (csv or txt)
+ * @param {string} fileType 
+ */
+async function run(fileType) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
+  const parsedData = { };
 
+  // fetch page data
   await page.goto(url);
   await page.waitForSelector('.message-list');
 
-  const boardTitle = await page.$eval('#board-name', (node) => node.innerText.trim());
+  // get board title
+  const boardTitle = await page.$eval('.board-name', (node) => node.innerText.trim());
 
-  if (!boardTitle) {
+  // add boardTitle value to parsedData object if it exists, if not throw an error
+  if (!boardTitle)
     throw 'Board title does not exist. Please check if provided URL is correct.'
-  }
+  else
+    parsedData.title = boardTitle;
 
-  let parsedText = boardTitle + '\n\n';
-
+  // prepare columns for parsing
   const columns = await page.$$('.message-list');
 
-  for (let i = 0; i < columns.length; i++) {
-    const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
+  /** 
+   * CONFIGURE DATA FOR CSV
+   * Expected output example:
+   * {
+   *    title: "My Board Contents",
+   *    data: [
+   *      ["col1", "col2", "col3"],
+   *      ["row1-col1", "row1-col2", "row1-col3"],
+   *      ["row2-col1", "row2-col2", "row2-col3"]
+   *    ]
+   * }
+   */
+  if (/csv/i.test(fileType)) {
+    // add empty data array to parsedData object
+    parsedData.data = [];
 
-    const messages = await columns[i].$$('.message-main');
-    if (messages.length) {
-      parsedText += columnTitle + '\n';
-    }
-    for (let i = 0; i < messages.length; i++) {
-      const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
-      const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
-      parsedText += `- ${messageText} (${votes})` + '\n';
-    }
+    // start looping through each column
+    for (let i = 0; i < columns.length; i++) {
+      // get column title
+      const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
 
-    if (messages.length) {
-      parsedText += '\n';
+      // add column titles to first child array
+      if (i === 0)
+        parsedData.data[0] = [columnTitle]; // initiate array with first item
+      else
+        parsedData.data[0].push(columnTitle); // push additional items to the first array
+
+      // get column messages
+      const messages = await columns[i].$$('.message-main');
+
+      // loop through and add messages to child arrays
+      for (let j = 0; j < messages.length; j++) {
+        let messageText = await messages[j].$eval('.message-body .text', (node) => node.innerText.trim());
+        const votes = await messages[j].$eval('.votes .vote-area span.show-vote-count', (node) => parseInt(node.innerText.trim()));
+
+        // check if an array (row) already exists
+        if (parsedData.data[(j+1)]) {
+          // add message to its respective position in the array based on column (i)
+          if (votes > 0)
+            parsedData.data[(j+1)][i] = messageText;
+        } else { // if an array does not exist then check if the message has more than 0 votes and create a new array
+          if (votes > 0) {
+            // create new array variable
+            let newArray = new Array(columns.length).fill('', 0, columns.length);
+
+            // then add content to its respective slot in the array
+            newArray[i] = messageText;
+
+            // finally push it into the parsedData.data array
+            parsedData.data.push(newArray);
+          }
+        }
+      }
     }
   }
 
-  return parsedText;
+  /**
+   * CONFIGURE DATA FOR PLAINTEXT
+   * Expected output example:
+   * {
+   *    title: "My Board Contents",
+   *    data: "My Board Contents\n\nCol 1 Title\n- Row 1-Col 1 Content (Vote Count)\n
+   *          - Row 2-Col 1 Content (Vote Count)\n\nCol 2 Title\n- Row 1-Col 2 Content (Vote Count)\n
+   *          - Row 2-Col 2 Content (Vote Count)"
+   * }
+   */
+  else if (/txt/i.test(fileType)) {
+    // start new parsedData.data string with boardTitle
+    parsedData.data = boardTitle + '\n\n';
+
+    // start looping through each column
+    for (let i = 0; i < columns.length; i++) {
+      // get column title
+      const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
+
+      // get column messages
+      const messages = await columns[i].$$('.message-main');
+
+      // if there are messages in the column then add the column title to parsedData.data string
+      if (messages.length)
+        parsedData.data += columnTitle + '\n';
+
+      // loop through and add messages from the column to the parsedData.data string after the column title
+      for (let i = 0; i < messages.length; i++) {
+        const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
+        const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
+        parsedData.data += `- ${messageText} (${votes})` + '\n';
+      }
+
+      // then add a new line in the string after the final message if messages are present in the column
+      if (messages.length)
+        parsedData.data += '\n';
+    }
+  }
+
+  // return parsedData object to be written to a file
+  return parsedData;
 }
 
-function writeToFile(filePath, data) {
-  const resolvedPath = path.resolve(filePath || `../${data.split('\n')[0].replace('/', '')}.txt`);
-  fs.writeFile(resolvedPath, data, (error) => {
-    if (error) {
-      throw error;
-    } else {
-      console.log(`Successfully written to file at: ${resolvedPath}`);
-    }
-    process.exit();
-  });
+/**
+ * Write parsedData object to a file whether it's CSV or plaintext
+ * @param {string} fileType 
+ * @param {object} data 
+ */
+function writeToFile(fileType, data) {
+  /* 
+   * Use board title for filename and remove all whitespace characters
+   * Make fileType string all lowercase in case of edge case where user may enter "cSv" or "Txt" 
+  */
+  const resolvedPath = path.resolve(`./${data.title.replace(/\s/g, '')}.${fileType.toLowerCase()}`);
+
+  // set variable for data content
+  const dataContent = data.data;
+
+  // write csv file using fast-csv
+  if (/csv/i.test(fileType)) {
+    csv.writeToPath(resolvedPath, dataContent)
+      .on('error', error => { console.error(error); process.exit(); })
+      .on('finish', () => { console.log(`Successfully written to file at: ${resolvedPath}`); process.exit(); })
+  }
+  // write plaintext file
+  else if (/txt/i.test(fileType)) {
+    fs.writeFile(resolvedPath, dataContent, (error) => {
+      if (error) {
+        throw error;
+      } else {
+        console.log(`Successfully written to file at: ${resolvedPath}`);
+      }
+      process.exit();
+    });
+  }
 }
 
 function handleError(error) {
   console.error(error);
 }
 
-run().then((data) => writeToFile(file, data)).catch(handleError);
+/**
+ * Initiate run function and pass the extension variable
+ * If run is successful then initiate writeToFile function and finish
+ * If run is not successful then throw an error
+ */
+run(extension).then((data) => writeToFile(extension, data)).catch(handleError);
